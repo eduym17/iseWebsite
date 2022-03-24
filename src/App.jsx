ab9783b069994205cf31221ee22500057282153b
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-03-23 21:20:12 -0600

    chore: add fonts and font weigths

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,45 @@ import React from 'react';
 function App() {
   return (
     <div className="bg-slate-900 text-slate-50 h-screen">
-      <h1>
-        Palabras iniciales del website
-      </h1>
+      <div className="Cabin">
+        <h1 className="font-Cabin font-regular text-5xl">
+          Cabin regular
+        </h1>
+        <h1 className="font-Cabin font-medium text-5xl">
+          Cabin medium
+        </h1>
+        <h1 className="font-Cabin font-semibold text-5xl">
+          Cabin semibold
+        </h1>
+        <h1 className="font-Cabin font-bold text-5xl">
+          Cabin bold
+        </h1>
+      </div>
+      <div className="Hero">
+        <h1 className="font-Hero font-light text-5xl">
+          Hero light
+        </h1>
+        <h1 className="font-Hero font-regular text-5xl">
+          Hero regular
+        </h1>
+        <h1 className="font-Hero font-bold text-5xl">
+          Hero bold
+        </h1>
+      </div>
+      <div className="Bariol">
+        <h1 className="font-Bariol font-thin text-5xl">
+          Bariol thin
+        </h1>
+        <h1 className="font-Bariol font-light text-5xl">
+          Bariol light
+        </h1>
+        <h1 className="font-Bariol font-regular text-5xl">
+          Bariol regular
+        </h1>
+        <h1 className="font-Bariol font-bold text-5xl">
+          Bariol bold
+        </h1>
+      </div>
     </div>
   );
 }
