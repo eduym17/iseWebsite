d898c135ad147c6d08c111d76b91e97a4bba7fb3
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-11 20:23:36 -0500

    chore: refactoring import and export files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import landingImage from './assets/images/landingImage.png';
-import logoInSitu from './assets/icons/logo-orange.png';
-import menuIcon from './assets/icons/menu-icon.png';
-import bbvaIcon from './assets/icons/clients/bbva-logo.png';
-import pemexIcon from './assets/icons/clients/pemex-logo.png';
-import toyotaIcon from './assets/icons/clients/toyota-logo.png';
-import electricaImgMobile from './assets/images/mobile-energia-electrica.png';
-import termicaImgMobile from './assets/images/mobile-energia-termica.png';
-import financiamientoIcon from './assets/icons/financiamiento-icon.png';
-import epcIcon from './assets/icons/epc-icon.png';
-import asesoriaIcon from './assets/icons/asesoria-icon.png';
-import exp15 from './assets/images/exp15.png';
-import exp30 from './assets/images/exp30.png';
-import exp35 from './assets/images/exp35.png';
-import arrowOpen from './assets/icons/arrow-open.png';
-import arrowClose from './assets/icons/arrow-close.png';
-import linkedinIcon from './assets/icons/linkedin-icon.png';
-import twitterIcon from './assets/icons/twitter-icon.png';
-import facebookIcon from './assets/icons/facebook-icon.png';
-import instagramIcon from './assets/icons/instagram-icon.png';
-import { ButtonMain, Paragraph, ServiceCard } from './components';
+import {
+  logoInSitu, menuIcon, pemexIcon, bbvaIcon, epcIcon, toyotaIcon,
+  financiamientoIcon, asesoriaIcon, arrowOpen, arrowClose, linkedinIcon,
+  twitterIcon, facebookIcon, instagramIcon,
+} from './components/icons';
+import {
+  landingImage, electricaImgMobile, termicaImgMobile, exp15, exp30, exp35,
+} from './components/images';
+import {
+  ButtonMain, Paragraph, ServiceCard,
+} from './components';
 
 function App() {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import ButtonMain from './ButtonMain';
 import Paragraph from './Paragraph';
 import ServiceCard from './ServiceCard';
+import Titles from './Titles';
 
-export { ButtonMain, Paragraph, ServiceCard };
+export {
+  ButtonMain, Paragraph, ServiceCard, Titles,
+};
