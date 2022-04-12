3e1c6c9fb3186fdba9c9c9872d1bacb922f70c99
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-11 20:22:21 -0500

    chore: js export files for icons and images

---
+++ src/components/icons.js
@@ -0,0 +1,20 @@
+import logoInSitu from '../assets/icons/logo-orange.png';
+import menuIcon from '../assets/icons/menu-icon.png';
+import pemexIcon from '../assets/icons/clients/pemex-logo.png';
+import bbvaIcon from '../assets/icons/clients/bbva-logo.png';
+import epcIcon from '../assets/icons/epc-icon.png';
+import toyotaIcon from '../assets/icons/clients/toyota-logo.png';
+import financiamientoIcon from '../assets/icons/financiamiento-icon.png';
+import asesoriaIcon from '../assets/icons/asesoria-icon.png';
+import arrowOpen from '../assets/icons/arrow-open.png';
+import arrowClose from '../assets/icons/arrow-close.png';
+import linkedinIcon from '../assets/icons/linkedin-icon.png';
+import twitterIcon from '../assets/icons/twitter-icon.png';
+import facebookIcon from '../assets/icons/facebook-icon.png';
+import instagramIcon from '../assets/icons/instagram-icon.png';
+
+export {
+  logoInSitu, menuIcon, pemexIcon, bbvaIcon, epcIcon, toyotaIcon,
+  financiamientoIcon, asesoriaIcon, arrowOpen, arrowClose, linkedinIcon,
+  twitterIcon, facebookIcon, instagramIcon,
+};
---
+++ src/components/images.js
@@ -0,0 +1,10 @@
+import landingImage from '../assets/images/landingImage.png';
+import electricaImgMobile from '../assets/images/mobile-energia-electrica.png';
+import termicaImgMobile from '../assets/images/mobile-energia-termica.png';
+import exp15 from '../assets/images/exp15.png';
+import exp30 from '../assets/images/exp30.png';
+import exp35 from '../assets/images/exp35.png';
+
+export {
+  landingImage, electricaImgMobile, termicaImgMobile, exp15, exp30, exp35,
+};
