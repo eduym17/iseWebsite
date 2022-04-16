7fa19af6e6fdd1aab77179a12f57a9e4cc1e27c3
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-15 20:19:58 -0500

    chore: add tablet and desktop behaviour

--- src/components/ExperienceItem.jsx
+++ src/components/ExperienceItem.jsx
@@ -5,9 +5,9 @@ function ExperienceItem({
   text, magicWord, image, customClass,
 }) {
   return (
-    <div className={`flex flex-col items-center ${customClass}`}>
-      <img src={image} alt={text} className="h-28 md:h-40" />
-      <p className="text-2xl font-bold text-center leading-none">
+    <div className={`flex flex-col items-center md:w-1/3 lg:w-2/5 ${customClass}`}>
+      <img src={image} alt={text} className="h-36 md:h-28 lg:h-40" />
+      <p className="text-2xl font-bold text-center leading-none lg:text-3xl">
         {text.split(' ').map((word) => (word === magicWord ? <span key={word} className="text-ise-orange">{`${magicWord} `}</span> : <span key={word}>{`${word} `}</span>))}
       </p>
     </div>
