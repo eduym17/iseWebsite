ed519a7cf845443c2f248d21e84c421d22e42e4c
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-15 20:16:40 -0500

    chore: add tablet and desktop behaviour

--- src/components/ClientsBar.jsx
+++ src/components/ClientsBar.jsx
@@ -4,7 +4,7 @@ import { pemexIcon, bbvaIcon, toyotaIcon } from './icons';
 
 function ClientsBar({ customClass }) {
   return (
-    <div className={`clientsBar w-full flex items-center justify-evenly ${customClass}`}>
+    <div className={`clientsBar w-full flex items-center justify-evenly md:justify-center md:gap-20 ${customClass}`}>
       <img src={toyotaIcon} alt="TOYOTA" className="h-10" />
       <img src={bbvaIcon} alt="BBVA" className="h-10" />
       <img src={pemexIcon} alt="PEMEX" className="h-10" />
