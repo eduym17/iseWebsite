37c3dc88fe2390ca01822d1435bb06846652737e
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-15 20:15:47 -0500

    chore: add desktop behaviour

--- src/components/ButtonMain.jsx
+++ src/components/ButtonMain.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function ButtonMain({ btnText, customClass }) {
   return (
-    <button type="button" className={`font-bold py-1.5 px-6 bg-ise-orange hover:bg-sungradient-o active:bg-sungradient-o text-ise-gray-10 hover:text-ise-white active:text-ise-white rounded-full ${customClass}`}>
+    <button type="button" className={`font-bold py-1.5 px-6 bg-ise-orange hover:bg-sungradient-o active:bg-sungradient-o text-ise-gray-10 hover:text-ise-white active:text-ise-white rounded-full lg:text-lg ${customClass}`}>
       {btnText}
     </button>
   );
