ad5e53c0ae8e6937a9c476b12a3c2f67b0047465
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-18 16:03:05 -0500

    chore: not found page routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import AboutUs from './views/AboutUs';
 import Solutions from './views/Solutions';
+import NotFound from './views/NotFound';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/soluciones" element={<Solutions />} />
         <Route path="/nosotros" element={<AboutUs />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
