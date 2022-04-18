7819ce3dc655a06e091268abe6697234eaf81259
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-18 17:44:23 -0500

    fix: handle height logic

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -3,10 +3,18 @@ import propTypes from 'prop-types';
 
 function Faqs({ question, children, customClass }) {
   const content = useRef(null);
-  const [height, setHeight] = useState('0');
-  const [faq, setFaq] = useState(false);
+  const [height, setHeight] = useState(0);
+  const [faq, setFaq] = useState(true);
+  const changeHeight = () => {
+    if (faq && height > 0) {
+      content.current.style.height = `${height}px`;
+    } else if (!faq && height > 0) {
+      content.current.style.height = 0;
+    }
+  };
   const handleClick = () => {
     setFaq(!faq);
+    changeHeight();
   };
 
   useEffect(() => {
@@ -14,18 +22,14 @@ function Faqs({ question, children, customClass }) {
   }, []);
 
   useEffect(() => {
-    if (faq) {
-      content.current.style.height = `${height}px`;
-    } else {
-      content.current.style.height = 0;
-    }
-  }, [faq]);
+    handleClick();
+  }, [height]);
 
   return (
-    <div className={`faqItem transition-all duration-500 w-[90%] py-2 px-2 ${faq ? 'bg-ise-orange text-ise-white rounded-lg' : 'bg-ise-gray-50 text-ise-gray-30 rounded-3xl'} ${customClass}`}>
+    <div className={`faqItem transition-all duration-500 w-[90%] py-2 px-2 ${!faq ? 'bg-ise-orange text-ise-white rounded-lg' : 'bg-ise-gray-50 text-ise-gray-30 rounded-3xl'} ${customClass}`}>
       <div className="itemTitle flex justify-between items-center">
         <p className="font-bold">{question}</p>
-        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => handleClick()} className={`h-6 transform transition-all duration-500 hover:cursor-pointer ${faq ? 'rotate-180' : 'rotate-0'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2.5}>
+        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => handleClick()} className={`h-6 transform transition-all duration-500 hover:cursor-pointer ${!faq ? 'rotate-180' : 'rotate-0'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2.5}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
         </svg>
       </div>
