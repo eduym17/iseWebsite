d542ed85c8041f51ef06b6ea5e124d9a6ef44261
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-18 19:46:43 -0500

    feat: add partial routing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import {
   logoInSitu, linkedinIcon, twitterIcon, facebookIcon, instagramIcon,
@@ -9,7 +10,7 @@ function Footer({ customClass }) {
     <div className="containerFull bg-ise-gray-20">
       <div className={`containerContent px-2 items-stretch divide-y-2 divide-gray-400 ${customClass}`}>
         <div className="flex items-center justify-between mb-1 md:mb-4">
-          <a href="/">
+          <a href="#header">
             <img src={logoInSitu} alt="In Situ Energía" className="h-10 lg:h-12" />
           </a>
           <div className="flex flex-col items-end gap-2 md:flex-row-reverse md:items-center md:gap-10">
@@ -28,18 +29,18 @@ function Footer({ customClass }) {
               </a>
             </div>
             <div className="router flex gap-2 text-sm">
-              <a href="/" className="hover:text-ise-orange">Soluciones</a>
-              <a href="/" className="hover:text-ise-orange">Nosotros</a>
-              <a href="/" className="hover:text-ise-orange">Proyectos</a>
-              <a href="/" className="hover:text-ise-orange">FAQ</a>
+              <Link to="/soluciones" className="hover:text-ise-orange">Soluciones</Link>
+              <Link to="/nosotros" className="hover:text-ise-orange">Nosotros</Link>
+              <a href="#projects" className="hover:text-ise-orange">Proyectos</a>
+              <a href="#faqs" className="hover:text-ise-orange">FAQs</a>
             </div>
           </div>
         </div>
         <div className="pt-1 flex flex-col md:flex-row md:justify-between md:items-center ">
           <div className="router flex text-sm items-center justify-around md:gap-4">
-            <a href="/" className="hover:text-ise-orange">Política de privacidad</a>
-            <a href="/" className="hover:text-ise-orange">Términos y condiciones</a>
-            <a href="/" className="hover:text-ise-orange">Log In</a>
+            <Link to="/" className="hover:text-ise-orange">Política de privacidad</Link>
+            <Link to="/" className="hover:text-ise-orange">Términos y condiciones</Link>
+            <Link to="/" className="hover:text-ise-orange">Log In</Link>
           </div>
           <div className="copyright pt-1 text-sm flex justify-center">
             <p>{`© Todos los derechos reservados. In Situ Energía, ${new Date().getFullYear()}.`}</p>
