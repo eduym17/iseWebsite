9b07d8143aa40f17a14e2c6d358ca326651d40fd
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-18 19:47:22 -0500

    refactor: props in component

--- src/components/ButtonMain.jsx
+++ src/components/ButtonMain.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ButtonMain({ btnText, customClass }) {
+function ButtonMain({ children, customClass }) {
   return (
     <button type="button" className={`font-bold py-1.5 px-6 bg-ise-orange hover:bg-sungradient-o active:bg-sungradient-o text-ise-gray-10 hover:text-ise-white active:text-ise-white rounded-full lg:text-lg ${customClass}`}>
-      {btnText}
+      {children}
     </button>
   );
 }
 
 ButtonMain.propTypes = {
-  btnText: PropTypes.string,
+  children: PropTypes.string,
   customClass: PropTypes.string,
 };
 ButtonMain.defaultProps = {
-  btnText: 'Texto aquí',
+  children: 'Texto aquí',
   customClass: '',
 };
 
