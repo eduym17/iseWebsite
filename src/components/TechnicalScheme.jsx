057d63f2efef20c8ad74bc35adc7d4271fb37365
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-19 21:04:17 -0500

    feat: technical schemes component for solutions page

---
+++ src/components/TechnicalScheme.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { Subtitles } from './index';
+
+function TechnicalScheme({
+  children, subtitleFooter, title, magicWord, customClassSubtitle, customClassParag,
+  customClass,
+}) {
+  return (
+    <div className={`flex flex-col items-center md:flex-row md:items-center md:justify-center md:gap-2 md:w-[90%] ${customClass}`}>
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3">
+        <Subtitles title={title} magicWord={magicWord} customClass={`md:w-full md:text-left ${customClassSubtitle}`} />
+        <p className="leading-none text-sm font-semibold text-center md:w-full md:text-left lg:text-base">
+          {subtitleFooter}
+        </p>
+      </div>
+      <p className={`md:whitespace-pre-line font-Hero text-xs text-justify w-[90%] mt-2 md:w-full lg:text-base ${customClassParag}`}>
+        {children}
+      </p>
+    </div>
+  );
+}
+
+TechnicalScheme.propTypes = {
+  children: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+  subtitleFooter: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+  title: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+  magicWord: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+  customClassSubtitle: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+  customClassParag: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+  customClass: propTypes.oneOfType([propTypes.string, propTypes.arrayOf(propTypes.string)]),
+};
+
+TechnicalScheme.defaultProps = {
+  children: '',
+  subtitleFooter: '',
+  title: '',
+  magicWord: '',
+  customClassSubtitle: '',
+  customClassParag: '',
+  customClass: '',
+};
+
+export default TechnicalScheme;
