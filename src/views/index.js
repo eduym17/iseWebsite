e734ead6801b24120f224543835dd100a5cb78b1
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-21 17:37:01 -0500

    chore: create views export file

---
+++ src/views/index.js
@@ -0,0 +1,8 @@
+import Home from './Home';
+import AboutUs from './AboutUs';
+import Solutions from './Solutions';
+import NotFound from './NotFound';
+
+export {
+  Home, AboutUs, Solutions, NotFound,
+};
