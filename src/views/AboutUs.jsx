5ada1c0d11ae6024885a956e04be99d3ed0cef0a
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-21 17:16:43 -0500

    fix: last paragraph margin bottom

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -42,7 +42,7 @@ function AboutUs() {
             <img src={landingImage} alt="sistema fotovoltaico" className="hidden w-4/6 max-w-xs md:flex md:w-1/3 lg:max-w-sm md:mt-2" />
             <div className="flex flex-col items-center md:w-1/2 lg:w-1/2">
               <img src={electricaImgMobile} alt="Energía eléctrica" className="md:hidden w-[90%] max-w-sm rounded-lg mt-4 lg:mt-10 lg:h-40 lg:w-fit" />
-              <Paragraph customClass="mt-3 mb-6 whitespace-pre-line text-justify md:w-full md:mt-4">
+              <Paragraph customClass="mt-3 mb-8 whitespace-pre-line text-justify md:w-full md:mt-4">
                 {`Somos una empresa profundamente comprometida con las causas sociales y el combate al cambio climático, estamos seguros de que podemos ayudar a dejar un mundo mejor para las siguientes generaciones.
                 
                 Actualmente nuestra plantilla está compuesta por 50% de integrantes del género femenino y 50% del género masculino. No tenemos duda de que los mejores resultados surgen cuando combinamos todos nuestros talentos.`}
