7ff1b29b36a2c96414ae8589decedd4c989df868
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 12:48:07 -0500

    chore: remove terms and conditions page, not needed now

--- src/views/index.js
+++ src/views/index.js
@@ -2,9 +2,8 @@ import Home from './Home';
 import AboutUs from './AboutUs';
 import Solutions from './Solutions';
 import PrivacyPolicy from './PrivacyPolicy';
-import TermsAndConditions from './TermsAndConditions';
 import NotFound from './NotFound';
 
 export {
-  Home, AboutUs, Solutions, PrivacyPolicy, TermsAndConditions, NotFound,
+  Home, AboutUs, Solutions, PrivacyPolicy, NotFound,
 };
