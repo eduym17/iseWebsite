554aec4acac7ee45eaa28d3bbe34256bf254a600
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 12:29:07 -0500

    chore: create privacy policy subtitles and texts

---
+++ src/components/texts/privacy.js
@@ -0,0 +1,28 @@
+const Privacy = [
+  {
+    title: '1.1 Tratamiento de datos y finalidades de su tratamiento',
+    info: 'In Situ Energía recopila y usa únicamente la información proporcionada en alguno de los formularios contenidos en este sitio web por parte El Usuario, con alguno de los siguientes propósitos: a. Fines de identificación, b. Fines estadísticos, c. Contacto vía correo electrónico o vía telefónica por parte de alguno de las o los integrantes de nuestro personal. Lo anterior con el fin de responder a la o las solicitides enviadas, igualmente para fines de administración interna, seguimiento y análisis estadístico. Los datos recolectados son protegidos por los principios marcados en el artículo 6 de la Ley: ilicitud, consentimiento, información, calidad, finalidad, lealtad, proporcionalidad y responsabilidad.',
+  },
+  {
+    title: '1.2 Transferencia de datos personales',
+    info: 'Los datos personales recolectados por nuestro sitio web jamás serán transferidos a terceros, de acuerdo con lo establecido en el punto 1. In Situ Energía podrá hacer uso de los servicios de un tercero para el manejo, administración y actualización de este sitio web con fines estadísticos y de marketing, sin embargo, incluso cuando un tercero realice dicha gestión, In Situ Energía será quien tenga control sobre la información personal recolectada, aun si la empresa presentara adquisición o venta de activos y fusión con terceros.\n El sitio web de In Situ Energía puede utilizar “Cookies” con fines de identificación, personalización en el acceso a este portal y monitoreo de su uso, únicamente con el propósito de mejorar sus condiciones de navegación. Las “Cookies” no recaban datos personales de El Usuario. Este portal pudiera contener enlaces a sitios web externos, de los cuales el contenido y políticas de privacidad no son responsabilidad de la empresa.',
+  },
+  {
+    title: '1.3 Derechos correspondientes al titular de los datos personales',
+    info: 'Cualquier comentario, duda, sugerencia o queja acerca de Protección de Datos Personales en el sitio web de In Situ Energía (www.in-situ.com.mx) deberá hacerse a través de la sección de contacto señalada en dicho portal.\n El Usuario de este sitio web está respaldado y podrá ejercer los derechos "ARCO" (Acceso, Cancelación, Rectificación y Oposición), a través de la sección de contacto previamente mencionada. Cada solicitud emitida deberá contener: nombre completo o razón social, además de domicilio, correo electrónico o algún otro medio para dar respuesta a su solicitud, al igual que los documentos que acrediten la identidad o, en su caso, la representación legal.\n Cada solicitud deberá contar con una descripción clara y precisa respecto de sobre cuales de los datos personales busca hacer ejercicio de los derechos “ARCO”, así como cualquier otro elemento que facilite la localización de dichos datos.',
+  },
+  {
+    title: '1.4 Datos personales sensibles',
+    info: 'Este sitio web no recolecta datos personales sensibles de los usuarios, sin embargo, si sucediera dicha situación, los medios para dar consentimiento por parte de El Usuario serán: firma autógrafa, firma electrónica o cualquier otro mecanismo para acreditación de identidad. Datos como discapacidades, tipos de discapacidad, enfermedades, padecimientos, estado de salud, estudios médicos, tratamientos médicos, lesiones, accidentes, estatura, peso, talla, afiliaciones a sindicatos o a organizaciones laborales, creencias religiosas, opiniones políticas y participación en procesos o problemas judiciales, serán tratados bajo las más estrictas medidas de confidencialidad para garantizar la seguridad de los mismos.',
+  },
+  {
+    title: '1.5 Cambios en el aviso de privacidad',
+    info: 'Todo cambio o modificación a este aviso de privacidad se comunicará a El Usuario, por lo menos 30 (treinta) días naturales previo a su publicación, mediante un correo electrónico enviado a la dirección registrada en este sitio web y/o publicando una nota o aviso visible en este sitio web. In Situ Energía no se responsabiliza de la recepción de dicho aviso cuando algún problema relacionado con la cuenta de correo electrónico registrada en este sitio web y la información no pueda llegar al destinatario por cuestiones ajenas a nuestra operación.\n In Situ Energía se reserva el derecho a cambiar este aviso de privacidad en cualquier momento bajo las Leyes y Jurisdicción de los Estados Unidos Mexicanos.',
+  },
+  {
+    title: '1.6 Cumplimiento de obligaciones',
+    info: 'Este aviso de privacidad cumple con los requisitos marcados por la Ley Federal de Protección de Datos Personales en Posesión de los Particulares en sus artículos 15 y 16.',
+  },
+];
+
+export default Privacy;
