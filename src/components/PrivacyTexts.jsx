adf374a84be15d16eb445dbeca260663df8ca61e
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 12:28:48 -0500

    chore: create privacy policy component for subtitles and texts

---
+++ src/components/PrivacyTexts.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { Subtitles, Paragraph } from '.';
+
+function PrivacyTexts({
+  title, magicWord, children, customClass, customClassParagraph,
+}) {
+  return (
+    <div className="containerContent">
+      <Subtitles title={title} magicWord={magicWord} customClass={`${customClass}`} />
+      <Paragraph customClass={`mt-2 text-justify whitespace-pre-line ${customClassParagraph}`}>
+        {children}
+      </Paragraph>
+    </div>
+  );
+}
+
+PrivacyTexts.propTypes = {
+  title: propTypes.string,
+  magicWord: propTypes.string,
+  children: propTypes.string,
+  customClass: propTypes.string,
+  customClassParagraph: propTypes.string,
+};
+
+PrivacyTexts.defaultProps = {
+  title: '',
+  magicWord: '',
+  children: '',
+  customClass: '',
+  customClassParagraph: '',
+};
+
+export default PrivacyTexts;
