c94c732cdaadcb08af814ae165ea9eee4f6ebe37
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 16:05:07 -0500

    feat: build responsive not found page

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import {
-  ScrollToTop,
+  ScrollToTop, Header, Footer, ButtonLink, Titles, Paragraph,
 } from '../components';
+import {
+  notFound,
+} from '../components/images';
 
 function NotFound() {
   return (
-    <h1>
+    <div className="text-ise-gray-30 bg-ise-gray-10 font-Cabin h-screen flex flex-col justify-between">
       <ScrollToTop />
-      NotFound page
-    </h1>
+      <Header />
+      <div id="landingPage" className="landingPage bg-ise-gray-10 containerFull">
+        <div className="containerContent lg:flex-row lg:items-center lg:justify-center gap-10">
+          <img src={notFound} alt="Not Found" className="w-3/4 max-w-xs lg:w-1/3" />
+          <div className="w-full md:w-1/2 flex flex-col items-center">
+            <Titles title="Página no encontrada" magicWord="Página" customClass="mt-5" />
+            <Paragraph customClass="mt-2 whitespace-pre-line lg:mb-4">
+              {`Lo sentimos pero no pudimos encontrar lo que buscabas.
+              ¡Vuelve a intentar desde nuestra página de inicio!`}
+            </Paragraph>
+            <ButtonLink to="/" customClass="my-8 lg:my-12">
+              Volver a Inicio
+            </ButtonLink>
+          </div>
+        </div>
+      </div>
+      <Footer customClass="my-5" />
+    </div>
   );
 }
 
