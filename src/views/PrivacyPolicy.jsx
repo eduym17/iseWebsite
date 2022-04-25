8c3b0b5ad907b9f3973b91649d9c418abce89159
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 12:29:39 -0500

    feat: implement privacy policy component and build page

--- src/views/PrivacyPolicy.jsx
+++ src/views/PrivacyPolicy.jsx
@@ -1,59 +1,38 @@
 import React from 'react';
 import {
-  Header, Footer, Titles, Subtitles, Paragraph, ButtonAnchor, ScrollToTop,
+  Header, Footer, Titles, Paragraph, ScrollToTop,
 } from '../components';
-import { landingImage, electricaImgMobile, termicaImgMobile } from '../components/images';
+import PrivacyTexts from '../components/PrivacyTexts';
+import Privacy from '../components/texts/privacy';
 
 function PrivacyPolicy() {
   return (
-    <div className="text-ise-gray-30 font-Cabin h-screen bg-red-300">
+    <div className="text-ise-gray-30 font-Cabin">
       <ScrollToTop />
       <Header />
       <div id="landingPage" className="landingPage bg-ise-gray-10 containerFull">
-        <div className="containerContent md:flex-row-reverse md:justify-center md:py-8 md:px-7">
-          <Titles title="POLÍTICA DE PRIVACIDAD" />
-          <img src={landingImage} alt="sistema fotovoltaico" className="w-4/6 max-w-xs lg:max-w-xl" />
-          <div className="flex flex-col items-center md:items-start lg:w-1/2 md:w-1/2">
-            <Paragraph customClass="mt-3 md:text-left md:w-5/6 lg:w-4/5">
-              Genera tu propia energía y comienza a ahorrar de inmediato,
-              {' '}
-              tenemos la solución que necesitas.
-            </Paragraph>
-            <a href="/#contacto">
-              <ButtonAnchor customClass="my-5">Ponte en contacto</ButtonAnchor>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="solutionsSection bg-ise-gray-20 containerFull">
-        <div className="containerContent">
-          <Titles title="Nuestras soluciones" magicWord="soluciones" customClass="mt-6 lg:mt-12" />
-          <Paragraph customClass="mt-1 lg:mt-2">
-            Tecnologías rentables que te permitirán generar ahorros con el poder del Sol.
+        <div className="containerContent pb-8 md:pb-10 md:px-7 lg:pb-12">
+          <Titles title="Política de privacidad" magicWord="privacidad" customClass="mt-5 lg:mt-6" />
+          <Paragraph customClass="mt-4 text-justify whitespace-pre-line lg:mb-4">
+            De conformidad con la Ley Federal de Protección de Datos personales en Posesión de
+            {' '}
+            los Particulares (en adelante la &quot;Ley&quot;), vigente en los Estados Unidos
+            {' '}
+            Mexicanos, hacemos de su conocimiento que In Situ Energía, con domicilio en Insurgentes
+            {' '}
+            Sur 615, Col. Nápoles, Benito Juárez, CDMX, C.P. 03810, podrá recolectar y usar
+            {' '}
+            información proporcionada por usted (en adelante &quot;El Usuario&quot;) en este sitio
+            {' '}
+            web.
           </Paragraph>
-          <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-2 lg:gap-8">
-            <div className="flex flex-col items-center md:w-2/5">
-              <img src={electricaImgMobile} alt="Energía eléctrica" className="w-[90%] max-w-sm rounded-lg mt-6 lg:mt-10 lg:h-40 lg:w-fit" />
-              <Subtitles title="Energía eléctrica" magicWord="eléctrica" customClass="mt-1 lg:mt-2" />
-              <Paragraph customClass="mt-1 lg:w-4/5">
-                Aprovecha techos, suelos y estacionamientos para generar tu
-                {' '}
-                propia energía eléctrica.
-              </Paragraph>
-            </div>
-            <div className="flex flex-col items-center md:w-2/5">
-              <img src={termicaImgMobile} alt="Energía eléctrica" className="w-[90%] max-w-sm rounded-lg mt-6 lg:mt-10 lg:h-40 lg:w-fit" />
-              <Subtitles title="Energía térmica" magicWord="térmica" customClass="mt-1 lg:mt-2" />
-              <Paragraph customClass="mt-1 lg:w-4/5">
-                Instala un sistema de calentamiento solar que te ayude a disminuir tu
-                {' '}
-                consumo de gas.
-              </Paragraph>
-            </div>
-          </div>
-          <a href="/soluciones">
-            <ButtonAnchor customClass="my-6 lg:mt-8 lg:mb-12">Conoce más</ButtonAnchor>
-          </a>
+          {
+            Privacy.map((subtitle) => (
+              <PrivacyTexts title={subtitle.title} key={subtitle.title} magicWord="" customClass="mt-8" customClassParagraph="mt-2">
+                {subtitle.info}
+              </PrivacyTexts>
+            ))
+          }
         </div>
       </div>
       <Footer customClass="my-5" />
