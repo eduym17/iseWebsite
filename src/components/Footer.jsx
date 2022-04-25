484df573636480eeba47601ad0d11884d44bc7f9
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 12:47:09 -0500

    chore: remove terms and conditions page, not needed now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,7 +39,6 @@ function Footer({ customClass }) {
         <div className="pt-1 flex flex-col md:flex-row md:justify-between md:items-center ">
           <div className="router flex text-sm items-center justify-around md:gap-4">
             <Link to="/privacidad" className="hover:text-ise-orange">Política de privacidad</Link>
-            <Link to="/terminos" className="hover:text-ise-orange">Términos y condiciones</Link>
             <Link to="/" className="hover:text-ise-orange hidden">Log In</Link>
           </div>
           <div className="copyright pt-1 text-sm flex justify-center">
