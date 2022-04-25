411de2bc592b4b11f3f718aeb0a17148d7e85d5d
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-25 16:04:42 -0500

    chore: add not found svg file

--- src/components/images.js
+++ src/components/images.js
@@ -4,7 +4,8 @@ import termicaImgMobile from '../assets/images/mobile-energia-termica.png';
 import exp15 from '../assets/images/exp15.png';
 import exp30 from '../assets/images/exp30.png';
 import exp35 from '../assets/images/exp35.png';
+import notFound from '../assets/images/not-found.svg';
 
 export {
-  landingImage, electricaImgMobile, termicaImgMobile, exp15, exp30, exp35,
+  landingImage, electricaImgMobile, termicaImgMobile, exp15, exp30, exp35, notFound,
 };
