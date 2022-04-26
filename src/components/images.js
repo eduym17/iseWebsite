1e03c2aed2af1d5e034a8091d3a89a374f9dee45
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-26 16:41:52 -0500

    chore: add all final images

--- src/components/images.js
+++ src/components/images.js
@@ -5,7 +5,29 @@ import exp15 from '../assets/images/exp15.png';
 import exp30 from '../assets/images/exp30.png';
 import exp35 from '../assets/images/exp35.png';
 import notFound from '../assets/images/not-found.svg';
+import LandingImg from '../assets/images/landing.png';
+import electricDeskImg from '../assets/images/desktop-home-electric.png';
+import electricMobImg from '../assets/images/mobile-home-electric.png';
+import thermalDeskImg from '../assets/images/desktop-home-thermal.png';
+import thermalMobImg from '../assets/images/mobile-home-thermal.png';
+import xp15 from '../assets/images/home-15.png';
+import xp30 from '../assets/images/home-30.png';
+import xp85 from '../assets/images/home-85.png';
+import electricSolutionsDeskImg from '../assets/images/desktop-solutions-electric.png';
+import electricSolutionsMobImg from '../assets/images/mobile-solutions-electric.png';
+import thermalSolutionsDeskImg from '../assets/images/desktop-solutions-thermal.png';
+import thermalSolutionsMobImg from '../assets/images/mobile-solutions-thermal.png';
+import storageSolutionsDeskImg from '../assets/images/desktop-solutions-storage.png';
+import storageSolutionsMobImg from '../assets/images/mobile-solutions-storage.png';
+import usDeskImg from '../assets/images/desktop-aboutus-us.png';
+import usMobImg from '../assets/images/mobile-aboutus-us.png';
+import commitmentDeskImg from '../assets/images/desktop-aboutus-commitment.png';
+import commitmentMobImg from '../assets/images/mobile-aboutus-commitment.png';
 
 export {
-  landingImage, electricaImgMobile, termicaImgMobile, exp15, exp30, exp35, notFound,
+  landingImage, electricaImgMobile, termicaImgMobile, exp15, exp30, exp35,
+  notFound, LandingImg, electricDeskImg, electricMobImg, thermalDeskImg, thermalMobImg,
+  xp15, xp30, xp85, electricSolutionsDeskImg, electricSolutionsMobImg, thermalSolutionsDeskImg,
+  thermalSolutionsMobImg, storageSolutionsDeskImg, storageSolutionsMobImg, usDeskImg, usMobImg,
+  commitmentDeskImg, commitmentMobImg,
 };
