0e07a60e27b515d0671be37c6bc57508f7825e1d
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-27 10:00:35 -0500

    chore: delete unused imports for icons

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,20 +1,23 @@
 import logoInSitu from '../assets/icons/logo-orange.png';
-import menuIcon from '../assets/icons/menu-icon.png';
-import pemexIcon from '../assets/icons/clients/pemex-logo.png';
-import bbvaIcon from '../assets/icons/clients/bbva-logo.png';
+import logoCanadianSolar from '../assets/icons/alliances/logo-canadianSolar.png';
+import logoKiwaPower from '../assets/icons/alliances/logo-kiwaPower.png';
+import logoPdcEnergy from '../assets/icons/alliances/logo-pdcEnergy.png';
+import logoTrinaSolar from '../assets/icons/alliances/logo-trinaSolar.png';
+import iconBbva from '../assets/icons/clients/icon-bbva.svg';
+import iconPemex from '../assets/icons/clients/icon-pemex.svg';
+import iconRecsa from '../assets/icons/clients/icon-recsa.svg';
+import iconRobles from '../assets/icons/clients/icon-robles.svg';
+import iconToyota from '../assets/icons/clients/icon-toyota.svg';
 import epcIcon from '../assets/icons/epc-icon.png';
-import toyotaIcon from '../assets/icons/clients/toyota-logo.png';
 import financiamientoIcon from '../assets/icons/financiamiento-icon.png';
 import asesoriaIcon from '../assets/icons/asesoria-icon.png';
-import arrowOpen from '../assets/icons/arrow-open.png';
-import arrowClose from '../assets/icons/arrow-close.png';
 import linkedinIcon from '../assets/icons/linkedin-icon.png';
 import twitterIcon from '../assets/icons/twitter-icon.png';
 import facebookIcon from '../assets/icons/facebook-icon.png';
 import instagramIcon from '../assets/icons/instagram-icon.png';
 
 export {
-  logoInSitu, menuIcon, pemexIcon, bbvaIcon, epcIcon, toyotaIcon,
-  financiamientoIcon, asesoriaIcon, arrowOpen, arrowClose, linkedinIcon,
-  twitterIcon, facebookIcon, instagramIcon,
+  logoInSitu, epcIcon, financiamientoIcon, asesoriaIcon, linkedinIcon, twitterIcon,
+  facebookIcon, instagramIcon, iconBbva, iconPemex, iconRecsa, iconRobles, iconToyota,
+  logoCanadianSolar, logoKiwaPower, logoPdcEnergy, logoTrinaSolar,
 };
