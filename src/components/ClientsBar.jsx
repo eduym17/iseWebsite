6bfce731a24e5b0d39296c168e7068875ddf2f78
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-27 09:57:09 -0500

    chore: implement clients icons and fix layout

--- src/components/ClientsBar.jsx
+++ src/components/ClientsBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { pemexIcon, bbvaIcon, toyotaIcon } from './icons';
+import {
+  iconPemex, iconRecsa, iconRobles, iconToyota,
+} from './icons';
 
 function ClientsBar({ customClass }) {
   return (
-    <div className={`clientsBar w-full flex items-center justify-evenly md:justify-center md:gap-20 ${customClass}`}>
-      <img src={toyotaIcon} alt="TOYOTA" className="h-10" />
-      <img src={bbvaIcon} alt="BBVA" className="h-10" />
-      <img src={pemexIcon} alt="PEMEX" className="h-10" />
+    <div className={`clientsBar w-full flex flex-wrap gap-10 gap-y-6 px-4 items-center justify-evenly md:justify-center md:gap-20 ${customClass}`}>
+      <img src={iconToyota} alt="Toyota" className="h-10" />
+      <img src={iconRobles} alt="Los Robles" className="h-10" />
+      <img src={iconPemex} alt="PEMEX" className="h-10" />
+      <img src={iconRecsa} alt="PEMEX" className="h-10" />
     </div>
   );
 }
