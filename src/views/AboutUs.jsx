4bfa275cd353ada2f0c7843ccd6a3ce8c4a0a753
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-27 09:59:23 -0500

    chore: update alliances icons and routes, fix desktop layout

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -5,7 +5,7 @@ import {
 import {
   usDeskImg, usMobImg, commitmentDeskImg, commitmentMobImg,
 } from '../components/images';
-import { bbvaIcon } from '../components/icons';
+import { logoCanadianSolar, logoPdcEnergy, logoTrinaSolar } from '../components/icons';
 
 function AboutUs() {
   return (
@@ -15,7 +15,7 @@ function AboutUs() {
       <div className="bg-ise-gray-10 containerFull">
         <div className="containerContent flex flex-col">
           <Titles title="Acerca de nosotros" magicWord="nosotros" customClass="mt-5 lg:mt-6" />
-          <div className="flex md:my-6 md:flex-row-reverse md:items-center md:justify-center md:gap-8 lg:mt-4 lg:mb-12">
+          <div className="flex md:my-6 md:flex-row-reverse md:items-center md:justify-center md:gap-8 lg:mt-4 lg:mb-12 lg:gap-16">
             <img src={usDeskImg} alt="sistema fotovoltaico" className="hidden w-4/6 max-w-xs md:flex md:w-1/3 lg:max-w-sm" />
             <div className="flex flex-col items-center md:w-1/2 lg:w-1/2">
               <img src={usMobImg} alt="Energía eléctrica" className="md:hidden w-[90%] max-w-sm rounded-lg mt-4 lg:mt-10 lg:h-40 lg:w-fit" />
@@ -32,16 +32,16 @@ function AboutUs() {
         <div className="containerContent flex flex-col md:py-2">
           <Titles title="Nuestras alianzas" magicWord="alianzas" customClass="mt-6 md:mt-4 lg:mt-8 md:text-center" />
           <div className="w-[90%] flex flex-col gap-y-6 gap-x-10 justify-center items-center mt-8 mb-8 lg:flex-row lg:flex-wrap lg:w-full lg:my-14 lg:gap-y-12">
-            <a href="/"><img src={bbvaIcon} alt="aliances" className="h-10 lg:h-14" /></a>
-            <a href="/"><img src={bbvaIcon} alt="aliances" className="h-10 lg:h-14" /></a>
-            <a href="/"><img src={bbvaIcon} alt="aliances" className="h-10 lg:h-14" /></a>
+            <a href="https://www.canadiansolar.com/la" target="_blank" rel="noreferrer"><img src={logoCanadianSolar} alt="Canadian Solar" className="h-10 lg:h-14" /></a>
+            <a href="https://pdcenergy.com.mx/" target="_blank" rel="noreferrer"><img src={logoPdcEnergy} alt="PDC Energy" className="h-10 lg:h-14" /></a>
+            <a href="https://www.trinasolar.com/lac" target="_blank" rel="noreferrer"><img src={logoTrinaSolar} alt="Trina Solar" className="h-10 lg:h-14" /></a>
           </div>
         </div>
       </div>
       <div className="bg-ise-gray-10 containerFull">
         <div className="containerContent flex flex-col md:py-4 lg:py-6">
           <Titles title="Nuestro compromiso" magicWord="compromiso" customClass="mt-6 md:mt-4 lg:mt-6" />
-          <div className="flex md:my-6 md:flex-row md:items-center md:justify-center md:gap-8 lg:mt-4">
+          <div className="flex md:my-6 md:flex-row md:items-center md:justify-center md:gap-8 lg:mt-4 lg:gap-16">
             <img src={commitmentDeskImg} alt="sistema fotovoltaico" className="hidden w-4/6 max-w-xs md:flex md:w-1/3 lg:max-w-sm md:mt-2" />
             <div className="flex flex-col items-center md:w-1/2 lg:w-1/2">
               <img src={commitmentMobImg} alt="Energía eléctrica" className="md:hidden w-[90%] max-w-sm rounded-lg mt-4 lg:mt-10 lg:h-40 lg:w-fit" />
