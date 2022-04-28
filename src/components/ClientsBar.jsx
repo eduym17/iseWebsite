e379d72bca1569e482f49bd4850c728cd2907d79
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-04-28 10:14:45 -0500

    fix: responsive heights

--- src/components/ClientsBar.jsx
+++ src/components/ClientsBar.jsx
@@ -7,10 +7,10 @@ import {
 function ClientsBar({ customClass }) {
   return (
     <div className={`clientsBar w-full flex flex-wrap gap-10 gap-y-6 px-4 items-center justify-evenly md:justify-center md:gap-20 ${customClass}`}>
-      <img src={iconToyota} alt="Toyota" className="h-10" />
-      <img src={iconRobles} alt="Los Robles" className="h-10" />
-      <img src={iconPemex} alt="PEMEX" className="h-10" />
-      <img src={iconRecsa} alt="PEMEX" className="h-10" />
+      <img src={iconToyota} alt="Toyota" className="h-10 md:h-12 lg:h-14" />
+      <img src={iconRobles} alt="Los Robles" className="h-10 md:h-12 lg:h-14" />
+      <img src={iconPemex} alt="PEMEX" className="h-10 md:h-12 lg:h-14" />
+      <img src={iconRecsa} alt="PEMEX" className="hidden h-10 md:h-12 lg:h-14" />
     </div>
   );
 }
