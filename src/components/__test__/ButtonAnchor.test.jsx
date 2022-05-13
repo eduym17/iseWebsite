0c803aff727dadb3eb98dcf96a27eb8756defae1
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-13 12:41:26 -0500

    refactor: data and imports

--- src/components/__test__/ButtonAnchor.test.jsx
+++ src/components/__test__/ButtonAnchor.test.jsx
@@ -1,34 +1,33 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ButtonAnchor from '../ButtonAnchor';
 import '@testing-library/jest-dom';
+import ButtonAnchor from '../ButtonAnchor';
+import { mockButtonData } from './mockData';
 
 // arrange
 // act
 // assert
 // npm test -- --coverage
 
-const mockChildren = 'Conoce más';
-const mockCustomClass = 'bg-violet-200';
-const mockHref = 'https://www.in-situ.com.mx/soluciones';
-
 describe('Button Anchor component', () => {
-  test('Should render', () => {
-    const button = render(
-      <ButtonAnchor href={mockHref} customClass={mockCustomClass}>
-        {mockChildren}
-      </ButtonAnchor>,
-    );
-    expect(button).toBeDefined();
-  });
-
-  test('Default values rendering', () => {
+  it('Render using default values', () => {
     const button = render(
       <ButtonAnchor />,
     );
     const buttonDefault = screen.getByText('Texto aquí');
     expect(button).toBeDefined();
     expect(buttonDefault).toBeInTheDocument();
+  });
+
+  it('Should render', () => {
+    const button = render(
+      <ButtonAnchor href={mockButtonData.mockHref} customClass={mockButtonData.mockCustomClass}>
+        {mockButtonData.mockChildren}
+      </ButtonAnchor>,
+    );
+    const buttonChildrenText = screen.getByText('Acerca de nosotros');
+    expect(buttonChildrenText).toBeInTheDocument();
+    expect(button).toBeDefined();
   });
 });
