6cdad0b6a9c0e6b2792d76c8b3927249b6124d1b
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-13 12:40:56 -0500

    feat: create mock data file

---
+++ src/components/__test__/mockData.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+const mockButtonData = {
+  mockChildren: 'Acerca de nosotros',
+  mockCustomClass: 'bg-blue-300',
+  mockHref: 'https://www.in-situ.com.mx/soluciones',
+  mockTo: '/nosotros',
+};
+
+export { mockButtonData };
