2c8bf1405374f060b9122b42c85640d6d4b98e8e
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-16 22:27:15 -0500

    test: render after submit

--- src/components/__test__/ContactForm.test.jsx
+++ src/components/__test__/ContactForm.test.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ContactForm from '../ContactForm';
 
@@ -12,8 +12,8 @@ describe('Contact Form component', () => {
     const phNameDefault = screen.getByPlaceholderText('Tu nombre*');
     const phLastNameDefault = screen.getByPlaceholderText('Tu apellido*');
     const phMailDefault = screen.getByPlaceholderText('Tu correo electrónico*');
-    const phPhoneDefault = screen.getByPlaceholderText('Tu correo electrónico*');
-    const phMessageDefault = screen.getByPlaceholderText('Tu correo electrónico*');
+    const phPhoneDefault = screen.getByPlaceholderText('Tu teléfono');
+    const phMessageDefault = screen.getByPlaceholderText('¡Haz una pregunta o comentario!');
     const submitButtonDefault = screen.getByText('Enviar');
     expect(contactForm).toBeDefined();
     expect(phNameDefault).toBeInTheDocument();
@@ -22,5 +22,24 @@ describe('Contact Form component', () => {
     expect(phPhoneDefault).toBeInTheDocument();
     expect(phMessageDefault).toBeInTheDocument();
     expect(submitButtonDefault).toBeInTheDocument();
+  });
+
+  it('Render after submitting', () => {
+    const { baseElement } = render(
+      <ContactForm />,
+    );
+    const phNameDefault = screen.getByPlaceholderText('Tu nombre*');
+    const phLastNameDefault = screen.getByPlaceholderText('Tu apellido*');
+    const phMailDefault = screen.getByPlaceholderText('Tu correo electrónico*');
+    const phPhoneDefault = screen.getByPlaceholderText('Tu teléfono');
+    const phMessageDefault = screen.getByPlaceholderText('¡Haz una pregunta o comentario!');
+    fireEvent.input(phNameDefault, { target: { value: 'Benny' } });
+    fireEvent.input(phLastNameDefault, { target: { value: 'García' } });
+    fireEvent.input(phMailDefault, { target: { value: 'benny.garcia@losreyes.com' } });
+    fireEvent.input(phPhoneDefault, { target: { value: '5512341234' } });
+    fireEvent.input(phMessageDefault, { target: { value: 'Una cosa es una cosa y otra cosa es otra cosa.' } });
+    fireEvent.submit(baseElement.firstChild.firstChild);
+    const successMsg = screen.getByText('¡Gracias por contactarnos!');
+    expect(successMsg).toBeInTheDocument();
   });
 });
