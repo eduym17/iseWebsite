f626386e746190b3e7704e387665fc0b49678dd1
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-18 20:34:57 -0500

    test: default render

---
+++ src/components/__test__/Footer.test.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Footer from '../Footer';
+import { mockFooter } from './__mockData__';
+
+const CLASS = 'containerContent px-3 md:px-4 items-stretch divide-y-2 divide-gray-400 my-5';
+
+jest.mock('../icons.js', () => ({
+  logoInSitu: '', linkedinIcon: '', twitterIcon: '', facebookIcon: '', instagramIcon: '',
+}));
+
+describe('Footer component', () => {
+  it('Should render links texts', () => {
+    const footer = render(
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>,
+    );
+    const defaultSolutions = screen.getByText('Soluciones');
+    const defaultCopyRight = screen.getByText('Política de privacidad');
+    const defaultLogIn = screen.getByText('Log In');
+    expect(footer).toBeDefined();
+    expect(defaultSolutions).toBeInTheDocument();
+    expect(defaultCopyRight).toBeInTheDocument();
+    expect(defaultLogIn).toBeInTheDocument();
+  });
+});
