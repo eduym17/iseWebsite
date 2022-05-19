ae36e1ff37c2f2e5330d8e6f709ef5b1b578fbe5
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-18 22:19:17 -0500

    test: default render

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { logoInSitu } from './icons';
 import ButtonAnchor from './ButtonAnchor';
 
 function Header() {
-  const [menu, setMenu] = useState('0');
+  const [menu, setMenu] = useState(true);
   const handleClick = () => {
     setMenu(!menu);
   };
@@ -28,7 +28,7 @@ function Header() {
         <Link to="/">
           <img src={logoInSitu} alt="In Situ Energía" className="h-9 lg:h-12" />
         </Link>
-        <button type="button" onKeyUp={(e) => enterHandlesMenu(e)} onBlur={(e) => blurHandler(e)} className={`cursor-default lg:hidden absolute right-3 top-3 transition-all duration-1000 ${menu ? 'text-ise-orange' : 'flex gap-8 bg-ise-orange py-1 pr-1 pl-3 rounded-lg text-ise-white font-semibold'}`}>
+        <button type="button" onKeyUp={(e) => enterHandlesMenu(e)} onBlur={(e) => blurHandler(e)} className={`cursor-default lg:hidden absolute right-3 top-3 transition-all duration-1000 ${menu ? 'text-ise-orange' : 'flex gap-8 bg-ise-orange py-1 pr-1 pl-3 rounded-lg text-ise-white font-semibold'}`} data-testid={`menuButton ${!menu}`}>
           <div className={`${menu ? 'hidden' : 'flex flex-col items-start'}`}>
             <Link to="/">Inicio</Link>
             <Link to="/soluciones">Soluciones</Link>
@@ -36,7 +36,7 @@ function Header() {
             <a href="/#proyectos">Proyectos</a>
             <a href="/#contacto">Contacto</a>
           </div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" onClick={() => handleClick()} className={`hover:cursor-pointer ${menu ? 'h-8 w-8' : 'h-6 w-6'}`}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" onClick={() => handleClick()} className={`hover:cursor-pointer ${menu ? 'h-8 w-8' : 'h-6 w-6'}`} data-testid={`svgButton ${!menu}`}>
             <path fillRule="evenodd" d={`${menu ? 'M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z' : 'M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z'} `} clipRule="evenodd" />
           </svg>
         </button>
---
+++ src/components/__test__/Header.test.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { screen, render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../Header';
+
+jest.mock('../icons.js', () => ({
+  logoInSitu: '',
+}));
+
+describe('Header component', () => {
+  it('Should render links texts', () => {
+    const header = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
+    const defaultSolutions = screen.getAllByText('Soluciones');
+    const defaultAboutUs = screen.getAllByText('Nosotros');
+    expect(header).toBeDefined();
+    expect(defaultSolutions[0]).toBeInTheDocument();
+    expect(defaultAboutUs[0]).toBeInTheDocument();
+  });
+});
