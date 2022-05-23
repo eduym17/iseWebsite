6071921fcad7d79a62cbcac7548e3dd7858ce382
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-23 18:21:48 -0500

    fix: unique keys, random value

--- src/components/Subtitles.jsx
+++ src/components/Subtitles.jsx
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 function Subtitles({ title, magicWord, customClass }) {
   return (
     <p className={`text-2xl font-bold text-center w-[90%] lg:text-3xl ${customClass}`}>
-      {title.split(' ').map((word) => (word === magicWord ? <span key={word} className="text-ise-orange">{`${magicWord} `}</span> : <span key={word}>{`${word} `}</span>))}
+      {title.split(' ').map((word) => (word === magicWord ? <span key={word + Math.random(5)} className="text-ise-orange">{`${magicWord} `}</span> : <span key={word + Math.random(5)}>{`${word} `}</span>))}
     </p>
   );
 }
