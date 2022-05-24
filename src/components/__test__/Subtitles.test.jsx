3c64b6a407876442e62d5f3324d8a1de3367e5f3
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-24 12:13:17 -0500

    test: subtitles component should render using default values

---
+++ src/components/__test__/Subtitles.test.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Subtitles from '../Subtitles';
+import { mockSubtitles } from './__mockData__';
+
+const CLASS_MAIN = 'text-2xl font-bold text-center w-[90%] lg:text-3xl';
+
+describe('Subtitles component', () => {
+  it('Should render using default values', () => {
+    const subtitle = render(
+      <Subtitles />,
+    );
+    const subtitleDefault = screen.getByText(/Subtítulo/i);
+    const parentEl = screen.getByText(/Subtítulo/i).parentElement;
+    expect(subtitle).toBeDefined();
+    expect(subtitleDefault).toBeInTheDocument();
+    expect(parentEl).toHaveClass(CLASS_MAIN);
+  });
+});
