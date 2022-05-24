3737bfa83131742066714fa5689884edb824106a
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-23 20:41:26 -0500

    test: project items component should add custom classes

--- src/components/__test__/ProjectItems.test.jsx
+++ src/components/__test__/ProjectItems.test.jsx
@@ -8,8 +8,10 @@ import { mockProjectItems } from './__mockData__';
 const CLASS_DESKTOP = 'hidden md:flex rounded-t-lg md:w-full';
 const CLASS_MOBILE = 'md:hidden max-w-sm rounded-t-lg w-[90%]';
 const CHILDREN_ONE = 'Llevamos hasta ti diferentes esquemas y fuentes de financiamiento, de acuerdo con las necesidades y requerimientos de tu proyecto.';
-const CUSTOM_CLASS = 'mt-6 md:mt-0';
-const CUSTOM_CLASS_TEXTS = 'mt-1';
+const MAIN_CLASS = 'flex flex-col items-center w-full hover:drop-shadow-lg lg:hover:drop-shadow-xl lg:w-[29%]';
+const CUSTOM_CLASS = `${MAIN_CLASS} mt-6 md:mt-0`;
+const MAIN_CLASS_TEXTS = 'bg-ise-gray-40 flex flex-col items-center rounded-b-lg w-[90%] px-2 py-2 max-w-sm md:w-full';
+const CUSTOM_CLASS_TEXTS = `${MAIN_CLASS_TEXTS} mt-1`;
 
 describe('Projec Items component', () => {
   it('Should render using default values', () => {
@@ -51,5 +53,20 @@ describe('Projec Items component', () => {
     );
     const projectItemText = screen.getByText(CHILDREN_ONE);
     expect(projectItemText).toBeInTheDocument();
+  });
+
+  it('Should change custom classes', () => {
+    render(
+      <ProjectItem
+        customClass={mockProjectItems.mockCustomClass}
+        customClassTexts={mockProjectItems.mockCustomClassTexts}
+      >
+        {mockProjectItems.mockChildren}
+      </ProjectItem>,
+    );
+    const projectItemTextParent = screen.getByText(CHILDREN_ONE).parentElement;
+    const parentEl = screen.getByText(CHILDREN_ONE).parentElement.parentElement;
+    expect(projectItemTextParent).toHaveClass(CUSTOM_CLASS_TEXTS);
+    expect(parentEl).toHaveClass(CUSTOM_CLASS);
   });
 });
