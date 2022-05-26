8f1f05cfbd091c72cb01bf9e029167f3af346acf
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-25 21:17:12 -0500

    chore: add route for log in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
-  Home, AboutUs, Solutions, PrivacyPolicy, NotFound,
+  Home, AboutUs, Solutions, PrivacyPolicy, NotFound, LogIn,
 } from './views';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Route path="/soluciones" element={<Solutions />} />
         <Route path="/nosotros" element={<AboutUs />} />
         <Route path="/privacidad" element={<PrivacyPolicy />} />
+        <Route path="/login" element={<LogIn />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
