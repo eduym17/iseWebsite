bd6390866fd802c1b65f0f049719edf7b62e309a
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-26 21:41:40 -0500

    feat: input number component

---
+++ src/components/InputNumber.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function InputText({
+  label, customPlaceholder, customClass, condition,
+}) {
+  return (
+    <div className={`py-1 ${customClass}`}>
+      <div className="flex justify-between items-center">
+        <label htmlFor={label} className="pl-1 font-medium">
+          {label}
+        </label>
+        <span className="pr-1 text-xs text-gray-500">
+          {condition}
+        </span>
+      </div>
+      <div className="mt-px">
+        <input
+          type="number"
+          step=".01"
+          name={label}
+          id={label}
+          placeholder={customPlaceholder}
+          className="w-full px-2 py-1 rounded-lg font-Hero shadow-sm border border-gray-400 focus:outline-none focus:border-ise-orange placeholder:text-sm appearance-none"
+        />
+      </div>
+    </div>
+  );
+}
+
+InputText.propTypes = {
+  label: PropTypes.string,
+  customPlaceholder: PropTypes.string,
+  customClass: PropTypes.string,
+  condition: PropTypes.string,
+};
+
+InputText.defaultProps = {
+  label: 'Label aquí',
+  customPlaceholder: 'Placeholder aquí',
+  customClass: '',
+  condition: '',
+};
+
+export default InputText;
