27fadb738b92c9ff1f7efdf6b252350945ad9314
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-26 21:25:10 -0500

    chore: add temporal input text components to log in page

--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Header, Footer, ScrollToTop, LogInForm,
+  Header, Footer, ScrollToTop, LogInForm, InputText,
 } from '../components';
 
 function Solutions() {
@@ -10,6 +10,12 @@ function Solutions() {
       <Header />
       <div className="bg-ise-gray-10 containerFullHeightFullWidth">
         <div className="containerContent h-full justify-center">
+          <div className="w-1/3">
+            <InputText label="Nombre" customPlaceholder="Nombre aquí" condition="Requerido" customClass="w-[50%]" />
+            <InputText label="Apellido" customPlaceholder="Apellido aquí" condition="Opcional" customClass="w-[75%]" />
+            <InputText label="E-mail" customPlaceholder="E-mail aquí" condition="Requerido" customClass="" />
+            <InputText label="Dirección" customPlaceholder="Calle, Número, Colonia, Municipio, Estado, CP" condition="Opcional" customClass="w-[125%]" />
+          </div>
           <LogInForm />
         </div>
       </div>
