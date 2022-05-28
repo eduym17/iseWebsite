ccfe4d18de1050255a7c063dafe5ab64e1d03697
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-27 21:17:09 -0500

    chore: create input select component

---
+++ src/components/calc/InputSelect.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function InputSelect({
+  label, customClass, isRequired, isDisabled,
+}) {
+  const entitiesMexico = [
+    'Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'CDMX', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo', 'Jalisco', 'Estado de México', 'Michoacán', 'Morelos', 'Nayarit', 'Nuevo León', 'Oaxaca', 'Puebla', 'Querétaro', 'Quintana Roo', 'San Luis Potosí', 'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatán', 'Zacatecas',
+  ];
+
+  return (
+    <div className={`py-1 ${customClass}`}>
+      <div className="flex justify-between items-center">
+        <label htmlFor={label} className="pl-1 font-medium">
+          {label}
+        </label>
+        <span className="pr-1 text-xs text-gray-500">
+          {isRequired === false ? 'Opcional' : 'Requerido'}
+        </span>
+      </div>
+      <div>
+        <select
+          name="select"
+          id={label}
+          className={`appearance-none w-full bg-ise-white px-2 py-1 rounded-lg font-Hero shadow-sm border focus:outline-none focus:border-ise-orange placeholder:text-sm disabled:cursor-not-allowed ${isDisabled === false ? 'border-gray-400' : 'opacity-50 border-gray-600'}`}
+          required={isRequired}
+          disabled={isDisabled}
+        >
+          <option selected disabled hidden>Elige una entidad</option>
+          {entitiesMexico.map((entity) => (
+            <option key={entity}>
+              {entity}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+InputSelect.propTypes = {
+  label: PropTypes.string,
+  customClass: PropTypes.string,
+  isRequired: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+};
+
+InputSelect.defaultProps = {
+  label: 'Label aquí',
+  customClass: '',
+  isRequired: false,
+  isDisabled: false,
+};
+export default InputSelect;
