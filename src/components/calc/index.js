283db24a3144413e606064a16b913daf4b5bbf88
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-27 21:13:11 -0500

    chore: add input select component to exports file

--- src/components/calc/index.js
+++ src/components/calc/index.js
@@ -1,7 +1,8 @@
 import InputText from './InputText';
 import InputNumber from './InputNumber';
 import InputCurrency from './InputCurrency';
+import InputSelect from './InputSelect';
 
 export {
-  InputText, InputNumber, InputCurrency,
+  InputText, InputNumber, InputCurrency, InputSelect,
 };
