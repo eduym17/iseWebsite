cd270dd6b9b7429fc550f48babc0ae5215e2a1fe
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-30 21:25:11 -0500

    feat: first calc routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
   Home, AboutUs, Solutions, PrivacyPolicy, NotFound, LogIn,
 } from './views';
+import Calcs from './layouts';
+import { CalcSelect, InfoClient, InfoSite } from './views/calc';
 
 function App() {
   return (
@@ -13,6 +15,11 @@ function App() {
         <Route path="/nosotros" element={<AboutUs />} />
         <Route path="/privacidad" element={<PrivacyPolicy />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/calcs" element={<Calcs />}>
+          <Route path="/calcs" element={<CalcSelect />} />
+          <Route path=":type/client-info" element={<InfoClient />} />
+          <Route path=":type/site-info" element={<InfoSite />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
