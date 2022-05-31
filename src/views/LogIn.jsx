8c2b382e8352f4783508cf2f6d75939d65dac8af
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-30 21:25:55 -0500

    refactor: login view layout

--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Header, Footer, ScrollToTop, LogInForm, InputText, InputNumber,
+  Header, Footer, ScrollToTop, LogInForm,
 } from '../components';
 
 function Solutions() {
@@ -8,15 +8,8 @@ function Solutions() {
     <div className="containerFullHeightFullWidth text-ise-gray-30 font-Cabin selection:bg-ise-orange selection:text-ise-white">
       <ScrollToTop />
       <Header />
-      <div className="bg-ise-gray-10 containerFullHeightFullWidth">
+      <div className="bg-ise-gray-10 containerScreenHeightFullWidth">
         <div className="containerContent h-full justify-center">
-          <div className="w-1/3">
-            <InputText label="Nombre" customPlaceholder="Nombre aquí" condition="Requerido" customClass="w-[50%]" />
-            <InputText label="Apellido" customPlaceholder="Apellido aquí" condition="Opcional" customClass="w-[75%]" />
-            <InputText label="E-mail" customPlaceholder="E-mail aquí" condition="Requerido" customClass="" />
-            <InputText label="Dirección" customPlaceholder="Calle, Número, Colonia, Municipio, Estado, CP" condition="Opcional" customClass="w-[125%]" />
-            <InputNumber label="Input número" customPlaceholder="0.00" condition="Opcional" customClass="w-[125%]" />
-          </div>
           <LogInForm />
         </div>
       </div>
