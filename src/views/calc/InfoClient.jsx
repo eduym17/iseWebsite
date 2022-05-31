5dae1bf59072c32a2314549a4f44dc14bf9f1041
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-30 22:33:37 -0500

    feat: input mapping

--- src/views/calc/InfoClient.jsx
+++ src/views/calc/InfoClient.jsx
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   InputText, InputNumber, InputCurrency, InputSelect,
 } from '../../components/calc';
+import fields from './fields.json';
 
 function InfoClient() {
+  const [infoClient, infoClientSet] = useState({});
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    infoClientSet({
+      ...infoClient, [name]: value,
+    });
+  };
+
   return (
     <div className="w-full flex flex-col items-center">
       <form className="md:w-2/3">
-        <InputText label="Nombre" customPlaceholder="Nombre aquí" isRequired customClass="" />
-        <InputText label="Apellido" customPlaceholder="Apellido aquí" isRequired customClass="" />
-        <InputText label="E-mail" customPlaceholder="E-mail aquí" isRequired customClass="" />
-        <InputText label="Dirección" customPlaceholder="Calle, Número, Colonia, Municipio, Estado, CP" isRequired customClass="" />
-        <InputNumber label="Input número 1" customPlaceholder="0.00" isRequired customClass="" />
-        <InputSelect label="Entidad" isRequired />
-        <InputNumber label="Input número 2" customPlaceholder="0.00" isRequired customClass="" />
-        <InputCurrency label="Precio 1" customPlaceholder="0.00" isRequired isDisabled customClass="" />
+        {fields['INFO-CLIENT'].map((field) => (
+          <SuperComponent field={field} infoClient={infoClient} onStateChange={onChange} />
+        ))}
         <button type="submit">Enviar</button>
       </form>
+      <pre>{JSON.stringify(infoClient)}</pre>
     </div>
   );
+}
+
+function SuperComponent({
+  field:{
+    type, label, placeholder, customClass, required, disabled, options,
+  }, infoClient, onStateChange
+}) {
+  if (type === 'text' || type === 'email' || type === 'tel') {
+    return (
+      <InputText
+        label={label}
+        customPlaceholder={placeholder}
+        customClass={customClass}
+        isRequired={required}
+        isDisabled={disabled}
+        type={type}
+        key={label}
+        onStateChangeProp={onStateChange}
+        val={infoClient[label]}
+      />
+    );
+  }
+  if (type === 'select') {
+    return (
+      <InputSelect
+        label={label}
+        customPlaceholder={placeholder}
+        customClass={customClass}
+        isRequired={required}
+        isDisabled={disabled}
+        type={type}
+        key={label}
+        options={options}
+        onStateChangeProp={onStateChange}
+        val={infoClient[label]}
+      />
+    );
+  }
 }
 
 export default InfoClient;
