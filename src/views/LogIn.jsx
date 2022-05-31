3f5145c7c02c6d40bcc8a13e81a14b16fcf0e27e
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-31 17:19:17 -0500

    fix: layout

--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -5,13 +5,11 @@ import {
 
 function Solutions() {
   return (
-    <div className="containerFullHeightFullWidth text-ise-gray-30 font-Cabin selection:bg-ise-orange selection:text-ise-white">
+    <div className="containerHeightScreenWidthFull bg-ise-gray-10 text-ise-gray-30 font-Cabin selection:bg-ise-orange selection:text-ise-white">
       <ScrollToTop />
       <Header />
-      <div className="bg-ise-gray-10 containerScreenHeightFullWidth">
-        <div className="containerContent h-full justify-center">
-          <LogInForm />
-        </div>
+      <div className="w-full flex justify-center items-center py-4">
+        <LogInForm />
       </div>
       <Footer customClass="my-5" />
     </div>
