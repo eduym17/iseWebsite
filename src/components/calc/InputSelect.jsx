db6a2aea6af5eb62ddc301cfd6d0cab9f76e7031
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-05-30 22:31:56 -0500

    refactor: add props to manage state

--- src/components/calc/InputSelect.jsx
+++ src/components/calc/InputSelect.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function InputSelect({
-  label, customClass, isRequired, isDisabled,
+  label, customClass, isRequired, isDisabled, options, val, onStateChangeProp,
 }) {
-  const entitiesMexico = [
-    'Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'CDMX', 'Durango', 'Guanajuato', 'Guerrero', 'Hidalgo', 'Jalisco', 'Estado de México', 'Michoacán', 'Morelos', 'Nayarit', 'Nuevo León', 'Oaxaca', 'Puebla', 'Querétaro', 'Quintana Roo', 'San Luis Potosí', 'Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatán', 'Zacatecas',
-  ];
-
   return (
     <div className={`py-1 ${customClass}`}>
       <div className="flex justify-between items-center">
@@ -20,14 +16,16 @@ function InputSelect({
       </div>
       <div>
         <select
-          name="select"
+          name={label}
           id={label}
           className={`appearance-none w-full bg-ise-white px-2 py-1 rounded-lg font-Hero shadow-sm border focus:outline-none focus:border-ise-orange placeholder:text-sm disabled:cursor-not-allowed ${isDisabled === false ? 'border-gray-400' : 'opacity-50 border-gray-600'}`}
           required={isRequired}
           disabled={isDisabled}
+          value={val}
+          onChange={(e) => onStateChangeProp(e)}
         >
           <option selected disabled hidden>Elige una entidad</option>
-          {entitiesMexico.map((entity) => (
+          {options.map((entity) => (
             <option key={entity}>
               {entity}
             </option>
@@ -43,6 +41,9 @@ InputSelect.propTypes = {
   customClass: PropTypes.string,
   isRequired: PropTypes.bool,
   isDisabled: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  val: PropTypes.string.isRequired,
+  onStateChangeProp: PropTypes.func.isRequired,
 };
 
 InputSelect.defaultProps = {
--- src/components/calc/InputText.jsx
+++ src/components/calc/InputText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function InputText({
-  label, customPlaceholder, customClass, isRequired, isDisabled,
+  label, customPlaceholder, customClass, isRequired, isDisabled, type, val, onStateChangeProp,
 }) {
   return (
     <div className={`py-1 ${customClass}`}>
@@ -16,13 +16,15 @@ function InputText({
       </div>
       <div className="mt-px">
         <input
-          type="text"
+          type={type}
           name={label}
           id={label}
           placeholder={customPlaceholder}
           className="w-full px-2 py-1 rounded-lg font-Hero shadow-sm border border-gray-400 focus:outline-none focus:border-ise-orange placeholder:text-sm disabled:cursor-not-allowed"
           required={isRequired}
           disabled={isDisabled}
+          value={val}
+          onChange={(e) => onStateChangeProp(e)}
         />
       </div>
     </div>
@@ -35,6 +37,9 @@ InputText.propTypes = {
   customClass: PropTypes.string,
   isRequired: PropTypes.bool,
   isDisabled: PropTypes.bool,
+  type: PropTypes.string.isRequired,
+  val: PropTypes.string.isRequired,
+  onStateChangeProp: PropTypes.func.isRequired,
 };
 
 InputText.defaultProps = {
