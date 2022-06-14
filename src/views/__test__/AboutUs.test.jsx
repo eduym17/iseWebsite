7f3dc6fb92cb13ff85f9586debf22d39800ab116
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-14 18:34:18 -0500

    test: about us renders images

--- src/views/__test__/AboutUs.test.jsx
+++ src/views/__test__/AboutUs.test.jsx
@@ -21,6 +21,9 @@ const MAIN_TEXT = 'Fundada en el año 2017, y con más de diez años de experien
 const CLASS_MAIN_TEXT = 'font-Hero text-sm text-center w-[90%] md:w-full lg:text-base mt-3 mb-6 whitespace-pre-line text-justify md:w-full md:mt-4';
 const SECONDARY_TEXT = 'Somos una empresa profundamente comprometida con las causas sociales y el combate al cambio climático, estamos seguros de que podemos ayudar a dejar un mundo mejor para las siguientes generaciones. Actualmente nuestra plantilla está compuesta por 50% de integrantes del género femenino y 50% del género masculino. No tenemos duda de que los mejores resultados surgen cuando combinamos todos nuestros talentos.';
 const CLASS_SECONDARY_TEXT = 'font-Hero text-sm text-center w-[90%] md:w-full lg:text-base mt-3 mb-8 whitespace-pre-line text-justify md:w-full md:mt-4';
+const CLASS_PV_ONE = 'hidden w-4/6 max-w-xs md:flex md:w-1/3 lg:max-w-sm';
+const CLASS_PV_TWO = `${CLASS_PV_ONE} md:mt-2`;
+const CLASS_ALLIANCES_IMG = 'h-10 lg:h-14';
 
 describe('About Us view', () => {
   it('Should render view', () => {
@@ -88,5 +91,28 @@ describe('About Us view', () => {
     expect(companyMainText).toHaveClass(CLASS_MAIN_TEXT);
     expect(companySecondaryText).toBeInTheDocument();
     expect(companySecondaryText).toHaveClass(CLASS_SECONDARY_TEXT);
+  });
+
+  it('Should render images', () => {
+    render(
+      <BrowserRouter>
+        <AboutUs />
+      </BrowserRouter>,
+    );
+    const pvImgOne = screen.getAllByAltText('sistema fotovoltaico')[0];
+    const pvImgTwo = screen.getAllByAltText('sistema fotovoltaico')[1];
+    const canadianSolarImg = screen.getByAltText('Canadian Solar');
+    const pdcEnergyImg = screen.getByAltText('PDC Energy');
+    const trinaSolarImg = screen.getByAltText('Trina Solar');
+    expect(pvImgOne).toBeInTheDocument();
+    expect(pvImgOne).toHaveClass(CLASS_PV_ONE);
+    expect(pvImgTwo).toBeInTheDocument();
+    expect(pvImgTwo).toHaveClass(CLASS_PV_TWO);
+    expect(canadianSolarImg).toBeInTheDocument();
+    expect(canadianSolarImg).toHaveClass(CLASS_ALLIANCES_IMG);
+    expect(pdcEnergyImg).toBeInTheDocument();
+    expect(pdcEnergyImg).toHaveClass(CLASS_ALLIANCES_IMG);
+    expect(trinaSolarImg).toBeInTheDocument();
+    expect(trinaSolarImg).toHaveClass(CLASS_ALLIANCES_IMG);
   });
 });
