3435fa3f5c4c7e83f95d758764a28c5e0ab519c9
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-15 20:21:38 -0500

    test: not found view renders buttons, titles, paragraphs and images

--- src/views/__test__/NotFound.test.jsx
+++ src/views/__test__/NotFound.test.jsx
@@ -16,6 +16,9 @@ const CLASS_INICIO_ONE = 'hidden';
 const CLASS_INICIO_TWO = 'hidden font-semibold text-lg lg:flex items-center gap-12';
 const CLASS_PRIVACY = 'hover:text-ise-orange';
 const CLASS_TWITTER = 'h-7';
+const CLASS_BUTTONLINK = 'font-bold text-center whitespace-nowrap py-1.5 px-6 bg-ise-orange hover:bg-sungradient-o active:bg-sungradient-o text-ise-gray-10 hover:text-ise-white active:text-ise-white rounded-full lg:text-lg my-8 lg:my-12';
+const PARAGRAPH_TEXT = 'Lo sentimos pero no pudimos encontrar lo que buscabas. ¡Vuelve a intentar desde nuestra página de inicio!';
+const CLASS_PARAGRAPH = 'font-Hero text-sm text-center w-[90%] md:w-full lg:text-base mt-2 whitespace-pre-line lg:mb-4';
 
 describe('Not Found view', () => {
   it('Should render', () => {
@@ -45,5 +48,49 @@ describe('Not Found view', () => {
     expect(privacyPolicyFooter).toHaveClass(CLASS_PRIVACY);
     expect(twitterIconFooter).toBeInTheDocument();
     expect(twitterIconFooter).toHaveClass(CLASS_TWITTER);
+  });
+
+  it('Should render buttons', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const buttonLinkText = screen.getByText('Volver a Inicio');
+    expect(buttonLinkText).toBeInTheDocument();
+    expect(buttonLinkText).toHaveClass(CLASS_BUTTONLINK);
+  });
+
+  it('Should render titles', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const titleText = screen.getAllByText(/Página/i)[0];
+    expect(titleText).toBeInTheDocument();
+    expect(titleText).toHaveClass('text-ise-orange');
+  });
+
+  it('Should render paragraphs', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const paragraphText = screen.getByText(PARAGRAPH_TEXT);
+    expect(paragraphText).toBeInTheDocument();
+    expect(paragraphText).toHaveClass(CLASS_PARAGRAPH);
+  });
+
+  it('Should render image', () => {
+    render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+    const notFoundImg = screen.getByAltText('Not Found');
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveClass('w-3/4 max-w-xs lg:w-1/3');
   });
 });
